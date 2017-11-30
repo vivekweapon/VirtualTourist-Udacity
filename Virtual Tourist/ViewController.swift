739ae92e7b1c1800568cf422b56ba05b4e9721cc
Vivekananda Cherukuri <vcherukuri25@gmail.com>
2017-11-30 13:03:49 +0530

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Vivekananda Cherukuri on 30/11/2017.
+//  Copyright © 2017 Flying Fish Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
